744d2819181f97e769fb78246bb4687ee79b04c0
Your Name <you@example.com>
2021-08-29 20:20:40 +0700

    first commit

--- ProjectShoukanshi/InsideForm/TambahSiswa.Designer.cs
+++ ProjectShoukanshi/InsideForm/TambahSiswa.Designer.cs
@@ -33,19 +33,20 @@
             this.textNIK = new System.Windows.Forms.TextBox();
             this.textNama = new System.Windows.Forms.TextBox();
             this.textKelas = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.dateTanggal = new System.Windows.Forms.DateTimePicker();
+            this.radioLaki = new System.Windows.Forms.RadioButton();
+            this.radioPerempuan = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textTempat = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textAlamat = new System.Windows.Forms.TextBox();
             this.btnSimpan = new System.Windows.Forms.Button();
             this.btnBatal = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.panelTmbhTitle.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -109,110 +110,112 @@
             this.textKelas.Size = new System.Drawing.Size(611, 27);
             this.textKelas.TabIndex = 3;
             // 
-            // dateTimePicker1
+            // dateTanggal
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 385);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 4;
+            this.dateTanggal.Location = new System.Drawing.Point(90, 386);
+            this.dateTanggal.Name = "dateTanggal";
+            this.dateTanggal.Size = new System.Drawing.Size(120, 20);
+            this.dateTanggal.TabIndex = 4;
             // 
-            // radioButton1
+            // radioLaki
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 257);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(64, 17);
-            this.radioButton1.TabIndex = 5;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Laki-laki";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioLaki.AutoSize = true;
+            this.radioLaki.Location = new System.Drawing.Point(12, 257);
+            this.radioLaki.Name = "radioLaki";
+            this.radioLaki.Size = new System.Drawing.Size(64, 17);
+            this.radioLaki.TabIndex = 5;
+            this.radioLaki.TabStop = true;
+            this.radioLaki.Text = "Laki-laki";
+            this.radioLaki.UseVisualStyleBackColor = true;
+            this.radioLaki.CheckedChanged += new System.EventHandler(this.radioLaki_CheckedChanged);
             // 
-            // radioButton2
+            // radioPerempuan
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(12, 280);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(79, 17);
-            this.radioButton2.TabIndex = 6;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Perempuan";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioPerempuan.AutoSize = true;
+            this.radioPerempuan.Location = new System.Drawing.Point(12, 280);
+            this.radioPerempuan.Name = "radioPerempuan";
+            this.radioPerempuan.Size = new System.Drawing.Size(79, 17);
+            this.radioPerempuan.TabIndex = 6;
+            this.radioPerempuan.TabStop = true;
+            this.radioPerempuan.Text = "Perempuan";
+            this.radioPerempuan.UseVisualStyleBackColor = true;
+            this.radioPerempuan.CheckedChanged += new System.EventHandler(this.radioPerempuan_CheckedChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(9, 56);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "NIK";
+            this.label1.Text = "NIK :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(9, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Nama";
+            this.label2.Text = "Nama :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(9, 164);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.Size = new System.Drawing.Size(39, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Kelas";
+            this.label3.Text = "Kelas :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(9, 232);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.Size = new System.Drawing.Size(77, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Jenis Kelamin";
+            this.label4.Text = "Jenis Kelamin :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(7, 320);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.Size = new System.Drawing.Size(75, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "Tempat Lahir";
+            this.label5.Text = "Tempat Lahir :";
             // 
-            // textBox1
+            // textTempat
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.textTempat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(0, 336);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(611, 27);
-            this.textBox1.TabIndex = 11;
+            this.textTempat.Location = new System.Drawing.Point(0, 336);
+            this.textTempat.Multiline = true;
+            this.textTempat.Name = "textTempat";
+            this.textTempat.Size = new System.Drawing.Size(611, 27);
+            this.textTempat.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 421);
+            this.label6.Location = new System.Drawing.Point(9, 421);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.Size = new System.Drawing.Size(90, 13);
             this.label6.TabIndex = 14;
-            this.label6.Text = "Tempat Lahir";
+            this.label6.Text = "Alamat Lengkap :";
             // 
-            // textBox2
+            // textAlamat
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.textAlamat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(0, 437);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(611, 55);
-            this.textBox2.TabIndex = 13;
+            this.textAlamat.Location = new System.Drawing.Point(0, 437);
+            this.textAlamat.Multiline = true;
+            this.textAlamat.Name = "textAlamat";
+            this.textAlamat.Size = new System.Drawing.Size(611, 55);
+            this.textAlamat.TabIndex = 13;
             // 
             // btnSimpan
             // 
@@ -225,6 +228,7 @@
             this.btnSimpan.TabIndex = 15;
             this.btnSimpan.Text = "Simpan";
             this.btnSimpan.UseVisualStyleBackColor = false;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
             // 
             // btnBatal
             // 
@@ -236,24 +240,34 @@
             this.btnBatal.UseVisualStyleBackColor = true;
             this.btnBatal.Click += new System.EventHandler(this.btnBatal_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 388);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Tanggal Lahir :";
+            // 
             // FormTambah
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(611, 546);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.btnBatal);
             this.Controls.Add(this.btnSimpan);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textAlamat);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textTempat);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radioPerempuan);
+            this.Controls.Add(this.radioLaki);
+            this.Controls.Add(this.dateTanggal);
             this.Controls.Add(this.textKelas);
             this.Controls.Add(this.textNama);
             this.Controls.Add(this.textNIK);
@@ -275,19 +289,20 @@
         private System.Windows.Forms.TextBox textNIK;
         private System.Windows.Forms.TextBox textNama;
         private System.Windows.Forms.TextBox textKelas;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DateTimePicker dateTanggal;
+        private System.Windows.Forms.RadioButton radioLaki;
+        private System.Windows.Forms.RadioButton radioPerempuan;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textTempat;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textAlamat;
         private System.Windows.Forms.Button btnSimpan;
         private System.Windows.Forms.Button btnBatal;
+        private System.Windows.Forms.Label label7;
 
     }
 }
--- ProjectShoukanshi/InsideForm/TambahSiswa.cs
+++ ProjectShoukanshi/InsideForm/TambahSiswa.cs
@@ -8,6 +8,9 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ProjectShoukanshi.InsideForm
 {
@@ -19,7 +22,8 @@ namespace ProjectShoukanshi.InsideForm
             this.Text = string.Empty;
             this.ControlBox = false;
         }
-       
+       string JenisKelamin;
+
         [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
         private extern static void ReleaseCapture();
         [DllImport("user32.DLL", EntryPoint = "SendMessage")]
@@ -39,6 +43,42 @@ namespace ProjectShoukanshi.InsideForm
         private void btnBatal_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string constring = @"Data Source=DESKTOP-NTD0N2N\PROJECTAIDEN01;Initial Catalog=DataShoukan;Integrated Security=True";
+            string Query = "insert into Siswa (NIK, nama, kelas, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat_lengkap) values('" + this.textNIK.Text + "' , '" + this.textNama.Text + "' , '" + this.textKelas.Text + "' , '" + JenisKelamin + "' , '" + this.textTempat.Text + "' , '" + dateTanggal.Value.Date.ToString("yyyyMMdd") + "' , '" + this.textAlamat.Text + "');  ";
+            SqlConnection conDatabase = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand(Query, conDatabase);
+            SqlDataReader myReader;
+            try
+            {
+                conDatabase.Open();
+                myReader = cmd.ExecuteReader();
+                MessageBox.Show("Data Berhasil Di Simpan");
+                while (myReader.Read())
+                {
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void radioLaki_CheckedChanged(object sender, EventArgs e)
+        {
+         JenisKelamin="Laki-laki";
+        }
+
+        private void radioPerempuan_CheckedChanged(object sender, EventArgs e)
+        {
+            JenisKelamin="Perempuan";
         }
     }
 }
